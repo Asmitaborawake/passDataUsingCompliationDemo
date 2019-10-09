40f2324f9c51d251ec550d625b01287331c38d4e
Asmita Borawake <asmita.borawake@Sneha-Sures-Mac.local>
2019-10-09 17:47:28 +0530

    psuh code

--- PassDataUsingCompliation/SecondViewController.swift
+++ PassDataUsingCompliation/SecondViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var passwordLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func homeClick(_ sender: Any) {
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        vc.compliation = { dict in
+            self.userNameLbl.text = dict["name"] as? String
+            self.passwordLbl.text = (dict["password"] as! String)
+            
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-    */
+    
+   
 
 }
--- PassDataUsingCompliation/ViewController.swift
+++ PassDataUsingCompliation/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    typealias compliationHandler = ([String:Any]) -> Void
+    
+    var compliation : compliationHandler?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func saveClick(_ sender: Any) {
+        
+        guard let username = userNameTextfield.text else {return}
+        guard let password = passwordTextField.text else   {return}
+        
+        let dict = ["name" : username, "password": password]
+        guard let compliationBlock = compliation else {return}
+        compliationBlock(dict)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
 
